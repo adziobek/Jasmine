a4ef13b49cd567c71928dbb0fd06915381b9493a
adziobek <adziobek@apreel.com>
2017-09-17 14:18:16 +0200

    add simple it tests

--- spec/OfferServiceSpec.js
+++ spec/OfferServiceSpec.js
@@ -8,6 +8,28 @@ describe('OfferService', function () {
         client = new Client('Mariusz');
         offer = new Offer(1, 20000);
         offerService = new OfferService();
-    })
+    });
 
+    it('test nothing matcher', function () {
+        expect().nothing();
+    });
+
+    it('test toBe matcher', function () {
+        expect(1).toBe(1);
+        expect(client).toBe(client);
+    });
+
+    it('test toBeCloseTo matcher', function () {
+        expect(44.44).toBeCloseTo(44.4, 1);
+    });
+
+    it('test toBeDefined matcher', function () {
+        expect(offer).toBeDefined();
+    });
+
+    it('test toContain matcher', function () {
+        var names = ['kamil', 'marcin', 'magda'];
+        expect('marcin').toContain('cin');
+        expect(names).toContain('magda');
+    });
 });
